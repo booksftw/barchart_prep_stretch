c4e27f92e4c9f5fbaa1c026342ff974bee35ec9c
Nicholas Zachary <nick@Nicholass-MacBook-Air.local>
2018-12-02 20:53:27 -0800

    progress

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// console.log('hello connected');
 //
 // function myFunc(str){
 //   console.log(str + 'Jarvis');
@@ -8,28 +7,126 @@
 // }
 
 // [module_require_name].bar.create()
+//
+//
+//
+///
+//
 
-// const bar = require('./bar-module');
+form1Data = {
+  amount:0
+}
 
-
-function myFunc() {
-
-  function bar(name, barValue) {
-    this.firstName = first;
-    this.varValue  = barValue;
-  }
-
-  function drawBarChart(dataARRAY, optionsOBJECT, elementHTMLSELECTOR ){
-
-  }
-
-  // function drawBarChart (data, options, element){}
-
+//Form Start
+//
+//
+//
+//
+//
+function onForm1Submit() {
   $(document).ready(function() {
 
-    $('h1').css('background-color', 'blue');
+    var amountOfBars = $('#form1 input')[0].value;
 
-    $('#mainContainer').append('<div class="bar"></div>');
+    // Hide first form and create the next one
+    $('#form1').css('display', 'none');
+
+    form1Data.amount = amountOfBars;
+    createForm('form2', amountOfBars);
+  });
+}
+
+function onForm2Submit() {
+  console.log('form 2 submit');
+  $(document).ready(function() {
+    var form2Input = $('#form2 input');
+    console.log(form2Input);
+    var barValue = [];
+
+    for (var i = 0; i < form2Input.length ; i++) {
+      var inputValue = form2Input[i].value;
+      barValue.push(form2Input);
+    }
+
+    // form2Inputs.forEach( (input) => {
+    //   console.log(input.value);
+    // });
+  });
+}
+
+function createForm(name, args) {
+
+  if (name === 'form2') {
+    $('#form2').css('display', 'inline-block');
+
+    let amountOfBars = args;
+    // Generate this many bars
+    console.log(amountOfBars, 'generate this many bars for form');
+
+    // Open tag
+    $('#barFormContainer').html(`
+      <p> Enter Values </p>
+      <form id="form2">
+    `)
+    // middle
+    for (let i = 0; i < amountOfBars; i++) {
+      // Dynamically create second form
+      $('#form2').append(`
+     
+        <!--Enter Values by Bar: <br>-->
+        bar ${i}:
+        <input type="number" name="bar">
+    `);
+    }
+
+    // Close tag
+    $('#form2').append(`
+     <button type="button" onclick="onForm2Submit()">Submit</button>
+     </form>
+     `)
+
+  }
+}
+
+// FORM END
+//
+//
+//
+
+
+
+// Get Data from barForm.js and call this function
+// In the mean time make dummy data to test.
+function drawBarChart(dataARRAY, optionsOBJECT, elementHTMLSELECTOR ){
+
+}
+
+function Bar(name, barValue) {
+  this.name = name;
+  this.barValue  = barValue;
+  this.barWidth = 200;
+  this.barHeight = 100;
+  this.barColour = 'black';
+  this.barLabelColour = 'silver';
+  this.barMarginSpacing = 15;
+  this.barTitle = '[Insert Name Here]';
+}
+
+function testAddBarGraph() {
+
+  // This is how you dynamically set up bar creation
+  // You may need to process it before inserting into DOM
+  // var bar1 = new Bar('test', 10);
+  
+  console.log('test add', form1Data.amount);
+
+
+  //Static inserting in
+  $(document).ready(function() {
+
+    var i = 1;
+    $('h1').css('background-color', 'blue');
+    $('#mainContainer').append(`<div class="bar${i}"></div>`);
 
   });
 }
